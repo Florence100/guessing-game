1797963685cbb2e77e7b731495795831ed7237b7
anastasia <anastasia.gribovskaia@gmail.com>
2022-08-12 22:17:35 +0300

    guessing-games

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,44 @@
-class GuessingGame {
+class GuessingGame {    
     constructor() {}
 
+    //этот метод принимает минимальное и максимальное значение диапазона чисел для угадывания
     setRange(min, max) {
+        this.min = min
+        this.max = max 
+    }
+    
+    midNumber;
 
+    //этот метод возвращает кандидата решения (вы делаете предположение на основе диапазона и предыдущих предположений)
+    guess(min=this.min, max=this.max) {
+        let midNumber = Math.floor((min + max + 1)/2);
+        return midNumber;
     }
 
-    guess() {
-
+    //этот метод вызывается, если предыдущий вызов предположения () вернул число, которое больше, чем ответ
+    lower() {        
+        let max = this.guess();
+        let min = this.min
+        this.setRange(min, max)
+        this.guess(min=this.min, max=this.max)
     }
 
-    lower() {
-
-    }
-
+    //этот метод вызывается, если предыдущий вызов функции предположения() вернул число, которое меньше, чем ответ
     greater() {
-
+        let min = this.guess();
+        let max = this.max;
+        this.setRange(min, max)
+        this.guess(min=this.min, max=this.max)
     }
 }
 
+
 module.exports = GuessingGame;
+
+
+        
+
+
+
+
+
